ae0bc722d828afb7db8f74ad8e09bc0c5c60fb4d
Terran Liu <38232187+TianrunLiu@users.noreply.github.com>
2018-11-09 12:17:01 -0500

    Update main.c

--- main.c
+++ main.c
@@ -9,25 +9,26 @@ static void sighandler(int signo){
 	if(SIGINT == signo){
 		char * hey = "hey, you haven't paid your respect yet!\n";	
 		printf("\n%s",hey);	
-		int file = open("errmsg.txt", O_CREAT | O_WRONLY, 0444);
+		int file = open("errmsg.txt", O_CREAT | O_APPEND, 0644);
 		write(file,hey,strlen(hey));
+		close(file);
 		exit(1);
 	}
 }
 
 static void sighandler2 (int signo){
 	if(signo == SIGUSR1){
-		printf("respect paid.\n");
+		printf("respect paid. ppid:%d\n",getppid());
 	}
 }
 
 int main(){
 
 	signal(SIGINT,sighandler);
-	//signal(SIGUSR1,sighandler2);
+	signal(SIGUSR1,sighandler2);
 
 	while(1){
-		printf("press f to pay respect\n");
+		printf("press f to pay respect. pid:%d\n",getpid());
 		sleep(1);
 	}
 	
